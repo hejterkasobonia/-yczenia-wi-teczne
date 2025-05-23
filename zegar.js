68d7eec5c5396c992c67f45b735a4599c68aa1d8
hejterkasobonia <juliks2010@gmail.com>
2025-05-23 11:04:08 +0200

    Delete zegar.js

--- zegar.js
+++
@@ -1,8 +0,0 @@
-function updateMessage() {
-    const now = new Date();
-    const data = now.toLocaleDateString();
-    const godzina = now.toLocaleTimeString();
-    document.getElementById('zegar').textContent = `Dziś: ${data} | Godzina: ${godzina}`;
-}
-setInterval(updateMessage, 1000);
-updateMessage();
